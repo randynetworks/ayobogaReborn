4b76b01c9ee196251ee917fed1e0a5b4cdcd1bf1
randynetworks <randynetworks@gmail.com>
2020-04-02 17:13:18 +0700

    MENAMPILKAN DATA KARTU BELAJAR MASAK DENGAN MODEL V.1

--- application/controllers/Belajar.php
+++ application/controllers/Belajar.php
@@ -2,13 +2,22 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Belajar extends CI_Controller {
-    
+	
+	public function __construct() {
+        parent::__construct();
+        $this->load->model('belajar_masak_model');
+        $this->load->helper('url_helper');
+
+    }
+	
+
 	public function belajar_masak()
 	{
+		$data['cards_masak'] = $this->belajar_masak_model->get_card_belajar();
         $this->load->helper('url');
 
 		$this->load->view('templates/header');
-		$this->load->view('belajar/belajar_masak');
+		$this->load->view('belajar/belajar_masak',$data);
         $this->load->view('templates/footer'); 
 
 	}
--- application/views/belajar/belajar_masak.php
+++ application/views/belajar/belajar_masak.php
@@ -1,1 +1,18 @@
-ini tampilan masak
+<div class="container sm-5 mt-3 mb-3">
+    <div class="text-center">
+		<h1 class="display-3">Sudah siap belajar masak?</h1>
+	</div>
+	<h6 class="bg-primary text-white pt-3 pb-3 text-center">Tenang aja, semua belajar dari awal kok, engga ada yang namanya telat!</h6>
+    <div class="card-deck row mt-5">
+        <?foreach ($cards_masak as $cardMasak) : ?>
+            <div class="col-md-4 mb-3 text-center">
+                <div class="card text-dark">
+                    <div class="card-body text-align">
+                        <h4 class="card-title"><a href="" class="hover-link"><?= $cardMasak['title']; ?></a></h4>
+                        <p class="card-text"><?= $cardMasak['sub_title']; ?></p>
+                    </div>
+                </div>
+            </div>
+        <?endforeach?>
+    </div>
+</div>
