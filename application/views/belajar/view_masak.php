065f1abb16d5484989d03b2f7348f40355a62b47
randynetworks <randynetworks@gmail.com>
2020-04-04 12:48:26 +0700

    MENAMBAHKAN FITUR ONE PAGE SERVICE

---
+++ application/views/belajar/view_masak.php
@@ -0,0 +1,8 @@
+<div class="container sm-5 mt-3 mb-3">
+    <div class="text-center">
+		<h1 class="display-3"><?= $cards_masak_item['title'];?></h1>
+        <br>
+	</div>
+	<h6 class="bg-primary text-white pt-3 pb-3 text-center"><?= $cards_masak_item['sub_title'];?></h6>
+
+</div>
