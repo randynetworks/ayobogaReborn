a6e5f5adb321d1086efb190a02c57b47deaff4f8
randynetworks <randynetworks@gmail.com>
2020-11-12 10:42:40 +0700

    fix topbar

--- application/views/templates/dashboard_topbar.php
+++ application/views/templates/dashboard_topbar.php
@@ -21,7 +21,7 @@
                 </a>
                 <!-- Dropdown - User Information -->
                 <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                    <a class="dropdown-item" href="<?= base_url('me') ?>">
+                    <a class="dropdown-item" href="<?= base_url('dashboard/me') ?>">
                         <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                         Profile
                     </a>
