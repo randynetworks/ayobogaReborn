73a9e7d065dece866c028b7be7d5667cc9834765
randynetworks <randynetworks@gmail.com>
2020-11-19 15:22:47 +0700

    fix dashboard

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,10 +1,33 @@
 <!-- Main Content -->
 <div id="content">
 	<!-- Begin Page Content -->
-	<div class="container-fluid">
+	<div class="container">
 
 		<!-- Page Heading -->
 		<h1 class="h3 mb-4 text-gray-800">Selamat Datang, <?= $user['name']; ?></h1>
+
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-4 bg-white p-3 rounded text-center shadow-sm m-3">
+					<h3 class="text-dark">Admin Profile</h3>
+					<a href="<?= base_url('/dashboard/me') ?>" class="btn btn-success">Ke Profile!</a>
+				</div>
+				<div class="col-md-4 bg-white p-3 rounded text-center shadow-sm m-3">
+					<h3 class="text-dark">List Admin Akun</h3>
+					<a href="<?= base_url('/dashboard/admin_list') ?>" class="btn btn-success">Manage disini!</a>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-md-4 bg-white p-3 rounded text-center shadow-sm m-3">
+					<h3 class="text-dark">Manage Menus</h3>
+					<a href="<?= base_url('/dashboard/menus') ?>" class="btn btn-success">Manage disini!</a>
+				</div>
+				<div class="col-md-4 bg-white p-3 rounded text-center shadow-sm m-3">
+					<h3 class="text-dark">Manage Materials</h3>
+					<a href="<?= base_url('/dashboard/materials') ?>" class="btn btn-success">Manage disini!</a>
+				</div>
+			</div>
+		</div>
 
 	</div>
 	<!-- /.container-fluid -->
